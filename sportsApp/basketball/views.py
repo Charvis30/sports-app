67b030ed348974bb62a488480cc6d8df8a5dcef6
Charvis30 <97851133+Charvis30@users.noreply.github.com>
2023-03-23 20:43:46 +0000

    api

--- sportsApp/basketball/views.py
+++ sportsApp/basketball/views.py
@@ -2,12 +2,12 @@ from django.http import HttpResponse
 import http.client
 
 def basketball_data(request):
-    conn = http.client.HTTPSConnection("nba-stats4.p.rapidapi.com")
+    conn = http.client.HTTPSConnection("livescore6.p.rapidapi.com")
     headers = {
-        'x-rapidapi-host': "nba-stats4.p.rapidapi.com",
+        'x-rapidapi-host': "livescore6.p.rapidapi.com",
         'x-rapidapi-key': "62179425a4mshc82870dfbd61b7cp115211jsne5fa10ef3b21"
         }
-    conn.request("GET", "/teams/?per_page=50&page=1", headers=headers)
+    conn.request("GET", "/leagues/v2/list?Category=basketball", headers=headers)
     res = conn.getresponse()
     data = res.read()
     return HttpResponse(data.decode("utf-8"))
