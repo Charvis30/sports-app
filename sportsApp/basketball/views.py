b6bd87bfa185bdb36b89791d0d25f91d44fe7e7f
Charvis30 <97851133+Charvis30@users.noreply.github.com>
2023-03-28 05:09:38 +0000

    nba teams api data into a table

--- sportsApp/basketball/views.py
+++ sportsApp/basketball/views.py
@@ -1,16 +1,32 @@
 from django.http import HttpResponse
-import http.client
+import http.client, json
 from django.shortcuts import render
 
 
 def basketball_data(request):
-    conn = http.client.HTTPSConnection("livescore6.p.rapidapi.com")
+    conn = http.client.HTTPSConnection("free-nba.p.rapidapi.com")
     headers = {
-        'x-rapidapi-host': "livescore6.p.rapidapi.com",
+        'x-rapidapi-host': "free-nba.p.rapidapi.com",
         'x-rapidapi-key': "62179425a4mshc82870dfbd61b7cp115211jsne5fa10ef3b21"
         }
-    conn.request("GET", "/matches/v2/list-by-league?Category=basketball&Ccd=nba", headers=headers)
+    conn.request("GET", "/teams?page=0", headers=headers)
     res = conn.getresponse()
     data = res.read()
-    context = {'api_data': data.decode("utf-8")}
+    team_data = json.loads(data.decode("utf-8"))['data']
+    context = {'api_data': data}
+
+#East and West conferences
+    east_teams = []
+    west_teams = []
+    for team in team_data:
+        if team['conference'] == 'East':
+            east_teams.append(team)
+        elif team['conference'] == 'West':
+            west_teams.append(team)
+
+    # Pass the lists as separate contexts to the HTML template
+    context = {
+        'east_teams': east_teams,
+        'west_teams': west_teams
+    }
     return render(request, 'basketball.html', context)
