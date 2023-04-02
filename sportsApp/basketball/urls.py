d3d4ed3ee381c169ee91444a0454283bc707083a
Charvis30 <97851133+Charvis30@users.noreply.github.com>
2023-04-02 20:25:32 +0000

    news api

--- sportsApp/basketball/urls.py
+++ sportsApp/basketball/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('basketball/', views.basketball_data, name='basketball_data'),
-    path('basketball/', views.get_games_data, name='get_games_data'),
+    path('games/', views.get_games_data, name='games'),
 ]
--- sportsApp/news/views.py
+++ sportsApp/news/views.py
@@ -1,25 +1,35 @@
-from django.http import HttpResponse
-import http.client, json
+import http.client
+import json
 from django.shortcuts import render
 
-# Create your views here.
-
-
 def news(request):
-    conn = http.client.HTTPSConnection("nba-latest-news.p.rapidapi.com")
-
-    headers = {
+    # NBA API
+    nba_conn = http.client.HTTPSConnection("nba-latest-news.p.rapidapi.com")
+    nba_headers = {
         'X-RapidAPI-Key': "62179425a4mshc82870dfbd61b7cp115211jsne5fa10ef3b21",
         'X-RapidAPI-Host': "nba-latest-news.p.rapidapi.com"
     }
+    nba_conn.request("GET", "/articles", headers=nba_headers)
+    nba_res = nba_conn.getresponse()
+    nba_data = nba_res.read()
+    nba_news_data = json.loads(nba_data.decode("utf-8"))
 
-    conn.request("GET", "/articles", headers=headers)
+    # NFL API
+    nfl_conn = http.client.HTTPSConnection("football-news-live1.p.rapidapi.com")
+    nfl_headers = {
+        'X-RapidAPI-Key': "62179425a4mshc82870dfbd61b7cp115211jsne5fa10ef3b21",
+        'X-RapidAPI-Host': "football-news-live1.p.rapidapi.com"
+    }
+    nfl_conn.request("GET", "/news", headers=nfl_headers)
+    nfl_res = nfl_conn.getresponse()
+    nfl_data = nfl_res.read()
+    nfl_news_data = json.loads(nfl_data.decode("utf-8"))
 
-    res = conn.getresponse()
-    data = res.read()
+    # Combine the data from both APIs into a single dictionary
+    context = {
+        'nba_news_data': nba_news_data,
+        'nfl_news_data': nfl_news_data
+    }
 
-    news_data = json.loads(data.decode("utf-8"))
-
-    context = {'news_data': news_data}
-
+    # Render the HTML template with the combined data
     return render(request, 'news.html', context)
