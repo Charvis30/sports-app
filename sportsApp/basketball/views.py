cbac5997de7dbdef8a913bede2ea9d2ccd4c9130
Charvis30 <97851133+Charvis30@users.noreply.github.com>
2023-04-03 00:05:08 +0000

    Live nba scored

--- sportsApp/basketball/views.py
+++ sportsApp/basketball/views.py
@@ -4,42 +4,23 @@ from django.shortcuts import render
 
 
 def basketball_data(request):
-    # NBA teams
-    conn = http.client.HTTPSConnection("free-nba.p.rapidapi.com")
-    headers = {
-        'x-rapidapi-host': "free-nba.p.rapidapi.com",
-        'x-rapidapi-key': "62179425a4mshc82870dfbd61b7cp115211jsne5fa10ef3b21"
-    }
-    conn.request("GET", "/teams?page=0", headers=headers)
-    res = conn.getresponse()
-    data = res.read()
-    team_data = json.loads(data.decode("utf-8"))['data']
-
-    # East and West conferences
-    east_teams = []
-    west_teams = []
-    for team in team_data:
-        if team['conference'] == 'East':
-            east_teams.append(team)
-        elif team['conference'] == 'West':
-            west_teams.append(team)
+    
 
     # NBA games
-    conn = http.client.HTTPSConnection("tank01-fantasy-stats.p.rapidapi.com")
+    conn = http.client.HTTPSConnection("odds.p.rapidapi.com")
     headers = {
         'X-RapidAPI-Key': "62179425a4mshc82870dfbd61b7cp115211jsne5fa10ef3b21",
-        'X-RapidAPI-Host': "tank01-fantasy-stats.p.rapidapi.com"
+        'X-RapidAPI-Host': "odds.p.rapidapi.com"
     }
-    conn.request("GET", "/getNBAGamesForDate?gameDate=20230402", headers=headers)
+    conn.request("GET", "/v4/sports/basketball_nba/scores", headers=headers)
     res = conn.getresponse()
     data = res.read()
     games_data = json.loads(data.decode("utf-8"))
 
     # Combine both contexts into one
     context = {
-        'east_teams': east_teams,
-        'west_teams': west_teams,
+        
         'games_data': games_data,
     }
-
+   
     return render(request, 'basketball.html', context)
