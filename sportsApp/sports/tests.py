a3cea046983ab32d34eb3508ae033e2f00dfa68f
Charvis30 <97851133+Charvis30@users.noreply.github.com>
2023-04-03 17:14:57 +0000

    api data test

--- sportsApp/sports/tests.py
+++ sportsApp/sports/tests.py
@@ -1,3 +1,40 @@
 from django.test import TestCase
+from django.urls import reverse
+
+
+
 
 # Create your tests here.
+class BaseballDataTestCase(TestCase):
+    def test_baseball_data(self):
+        url = reverse('baseball')
+        response = self.client.get(url)
+        
+        # Check if the response contains the expected data
+        self.assertIn('games_data', response.context)
+        self.assertIn('odds_data', response.context)
+        self.assertTemplateUsed(response, 'baseball.html')
+
+
+class BasketballDataTestCase(TestCase):
+    def test_baseball_data(self):
+        url = reverse('basketball')
+        response = self.client.get(url)
+        
+        # Check if the response contains the expected data
+        self.assertIn('games_data', response.context)
+        self.assertIn('odds_data', response.context)
+        self.assertTemplateUsed(response, 'basketball.html')
+
+
+class HockeyDataTestCase(TestCase):
+    def test_baseball_data(self):
+        url = reverse('hockey')
+        response = self.client.get(url)
+        
+        # Check if the response contains the expected data
+        self.assertIn('games_data', response.context)
+        self.assertIn('odds_data', response.context)
+        self.assertTemplateUsed(response, 'hockey.html')
+
+
