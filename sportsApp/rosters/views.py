a7a9085ce64b37e4906692c9311b19987855d721
Charvis30 <97851133+Charvis30@users.noreply.github.com>
2023-04-26 16:09:52 +0000

    team roster updates

--- sportsApp/rosters/views.py
+++ sportsApp/rosters/views.py
@@ -34,7 +34,7 @@ def boston_roster(request):
     boston_roster = json.loads(data.decode("utf-8"))
 
     context = {'boston_roster': boston_roster}
-    return render(request, 'celtics.html', context)
+    return render(request, 'nba-teams/celtics.html', context)
 
 def brooklyn_roster(request):
     # Brooklyn Nets
@@ -49,7 +49,7 @@ def brooklyn_roster(request):
     brooklyn_roster = json.loads(data.decode("utf-8"))
 
     context = {'brooklyn_roster': brooklyn_roster}
-    return render(request, 'nets.html', context)
+    return render(request, 'nba-teams/nets.html', context)
 
 def ny_roster(request):
     # NY Knicks
@@ -64,7 +64,7 @@ def ny_roster(request):
     ny_roster = json.loads(data.decode("utf-8"))
 
     context = {'ny_roster': ny_roster}
-    return render(request, 'knicks.html', context)
+    return render(request, 'nba-teams/knicks.html', context)
 
 def philly_roster(request):
     # Philly 76ers
@@ -79,7 +79,7 @@ def philly_roster(request):
     philly_roster = json.loads(data.decode("utf-8"))
 
     context = {'philly_roster': philly_roster}
-    return render(request, 'sixers.html', context)
+    return render(request, 'nba-teams/sixers.html', context)
 
 def toronto_roster(request):
     # Toronto Raptors
@@ -94,7 +94,7 @@ def toronto_roster(request):
     toronto_roster = json.loads(data.decode("utf-8"))
 
     context = {'toronto_roster': toronto_roster}
-    return render(request, 'raptors.html', context)
+    return render(request, 'nba-teams/raptors.html', context)
 
 def chicago_roster(request):
     # Chicago Bulls
@@ -109,7 +109,7 @@ def chicago_roster(request):
     chicago_roster = json.loads(data.decode("utf-8"))
 
     context = {'chicago_roster': chicago_roster}
-    return render(request, 'bulls.html', context)
+    return render(request, 'nba-teams/bulls.html', context)
 
 def cleveland_roster(request):
     # Cleveland Cavs
@@ -124,7 +124,7 @@ def cleveland_roster(request):
     cleveland_roster = json.loads(data.decode("utf-8"))
 
     context = {'cleveland_roster': cleveland_roster}
-    return render(request, 'cavs.html', context)
+    return render(request, 'nba-teams/cavs.html', context)
 
 def detroit_roster(request):
     # detroit pistons
@@ -139,7 +139,7 @@ def detroit_roster(request):
     detroit_roster = json.loads(data.decode("utf-8"))
 
     context = {'detroit_roster': detroit_roster}
-    return render(request, 'pistons.html', context)
+    return render(request, 'nba-teams/pistons.html', context)
 
 def milwaukee_roster(request):    
     # Milwaukee Bucks
@@ -154,7 +154,7 @@ def milwaukee_roster(request):
     milwaukee_roster = json.loads(data.decode("utf-8"))
 
     context = {'milwaukee_roster': milwaukee_roster}
-    return render(request, 'bucks.html', context)
+    return render(request, 'nba-teams/bucks.html', context)
 
 def indiana_roster(request):
     # Indiana Pacers
@@ -169,7 +169,7 @@ def indiana_roster(request):
     indiana_roster = json.loads(data.decode("utf-8"))
 
     context = {'indiana_roster': indiana_roster}
-    return render(request, 'pacers.html', context)
+    return render(request, 'nba-teams/pacers.html', context)
 
 def atlanta_roster(request):
     # Atlanta Hawks
@@ -184,7 +184,7 @@ def atlanta_roster(request):
     atlanta_roster = json.loads(data.decode("utf-8"))
 
     context = {'atlanta_roster': atlanta_roster}
-    return render(request, 'hawks.html', context)
+    return render(request, 'nba-teams/hawks.html', context)
 
 def charlotte_roster(request):
     # charlotte hornets
@@ -199,7 +199,7 @@ def charlotte_roster(request):
     charlotte_roster = json.loads(data.decode("utf-8"))
 
     context = {'charlotte_roster': charlotte_roster}
-    return render(request, 'hornets.html', context)
+    return render(request, 'nba-teams/hornets.html', context)
 
 def miami_roster(request):
     # miami heat
@@ -214,7 +214,7 @@ def miami_roster(request):
     miami_roster = json.loads(data.decode("utf-8"))
 
     context = {'miami_roster': miami_roster}
-    return render(request, 'heat.html', context)
+    return render(request, 'nba-teams/heat.html', context)
 
 def orlando_roster(request):
 
@@ -230,7 +230,7 @@ def orlando_roster(request):
     orlando_roster = json.loads(data.decode("utf-8"))
 
     context = {'orlando_roster': orlando_roster}
-    return render(request, 'magic.html', context)
+    return render(request, 'nba-teams/magic.html', context)
 
 def washington_roster(request):
     # washington wizards
@@ -245,7 +245,7 @@ def washington_roster(request):
     washington_roster = json.loads(data.decode("utf-8"))
 
     context = {'washington_roster': washington_roster}
-    return render(request, 'wizards.html', context)
+    return render(request, 'nba-teams/wizards.html', context)
 
     ##WEST##
 
@@ -262,7 +262,7 @@ def la_roster(request):
     la_roster = json.loads(data.decode("utf-8"))
 
     context = {'la_roster': la_roster}
-    return render(request, 'lakers.html', context)
+    return render(request, 'nba-teams/lakers.html', context)
 
 def goldenstate_roster(request):
     # Golden State 
@@ -277,7 +277,7 @@ def goldenstate_roster(request):
     goldenstate_roster = json.loads(data.decode("utf-8"))
 
     context = {'goldenstate_roster': goldenstate_roster}
-    return render(request, 'warriors.html', context)
+    return render(request, 'nba-teams/warriors.html', context)
 
 def sacramento_roster(request):
     # sacramento 
@@ -292,7 +292,7 @@ def sacramento_roster(request):
     sacramento_roster = json.loads(data.decode("utf-8"))
 
     context = {'sacramento_roster': sacramento_roster}
-    return render(request, 'kings.html', context)
+    return render(request, 'nba-teams/kings.html', context)
 
 def lac_roster(request):
     # lac 
@@ -307,7 +307,7 @@ def lac_roster(request):
     lac_roster = json.loads(data.decode("utf-8"))
 
     context = {'lac_roster': lac_roster}
-    return render(request, 'clippers.html', context)
+    return render(request, 'nba-teams/clippers.html', context)
 
 def phoenix_roster(request):
     # phoenix 
@@ -322,7 +322,7 @@ def phoenix_roster(request):
     phoenix_roster = json.loads(data.decode("utf-8"))
 
     context = {'phoenix_roster': phoenix_roster}
-    return render(request, 'suns.html', context)
+    return render(request, 'nba-teams/suns.html', context)
 
 def denver_roster(request):
     # denver
@@ -337,7 +337,7 @@ def denver_roster(request):
     denver_roster = json.loads(data.decode("utf-8"))
 
     context = {'denver_roster': denver_roster}
-    return render(request, 'nuggets.html', context)
+    return render(request, 'nba-teams/nuggets.html', context)
 
 def minnesota_roster(request):
     #minnesota
@@ -352,7 +352,7 @@ def minnesota_roster(request):
     minnesota_roster = json.loads(data.decode("utf-8"))
 
     context = {'minnesota_roster': minnesota_roster}
-    return render(request, 'twolves.html', context)
+    return render(request, 'nba-teams/twolves.html', context)
 
 def okc_roster(request):
     #okc
@@ -367,7 +367,7 @@ def okc_roster(request):
     okc_roster = json.loads(data.decode("utf-8"))
 
     context = {'okc_roster': okc_roster}
-    return render(request, 'thunder.html', context)
+    return render(request, 'nba-teams/thunder.html', context)
 
 def portland_roster(request):
     #portland
@@ -382,7 +382,7 @@ def portland_roster(request):
     portland_roster = json.loads(data.decode("utf-8"))
 
     context = {'portland_roster': portland_roster}
-    return render(request, 'blazers.html', context)
+    return render(request, 'nba-teams/blazers.html', context)
 
 def utah_roster(request):
     #utah
@@ -397,7 +397,7 @@ def utah_roster(request):
     utah_roster = json.loads(data.decode("utf-8"))
 
     context = {'utah_roster': utah_roster}
-    return render(request, 'jazz.html', context)
+    return render(request, 'nba-teams/jazz.html', context)
 
 def dallas_roster(request):
     #dallas
@@ -412,7 +412,7 @@ def dallas_roster(request):
     dallas_roster = json.loads(data.decode("utf-8"))
 
     context = {'dallas_roster': dallas_roster}
-    return render(request, 'mavs.html', context)
+    return render(request, 'nba-teams/mavs.html', context)
 
 def houston_roster(request):
     #houston
@@ -427,7 +427,7 @@ def houston_roster(request):
     houston_roster = json.loads(data.decode("utf-8"))
 
     context = {'houston_roster': houston_roster}
-    return render(request, 'rockets.html', context)
+    return render(request, 'nba-teams/rockets.html', context)
 
 def memphis_roster(request):
     #memphis
@@ -442,7 +442,7 @@ def memphis_roster(request):
     memphis_roster = json.loads(data.decode("utf-8"))
 
     context = {'memphis_roster': memphis_roster}
-    return render(request, 'grizz.html', context)
+    return render(request, 'nba-teams/grizz.html', context)
 
 def san_roster(request):
     #san antonio
@@ -457,7 +457,7 @@ def san_roster(request):
     san_roster = json.loads(data.decode("utf-8"))
 
     context = {'san_roster': san_roster}
-    return render(request, 'spurs.html', context)
+    return render(request, 'nba-teams/spurs.html', context)
 
 def neworleans_roster(request):
     #new orleans
@@ -472,7 +472,7 @@ def neworleans_roster(request):
     neworleans_roster = json.loads(data.decode("utf-8"))
 
     context = {'neworleans_roster': neworleans_roster}
-    return render(request, 'pelicans.html', context)
+    return render(request, 'nba-teams/pelicans.html', context)
 
 
    
