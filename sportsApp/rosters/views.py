4fcef63b3cf09ad7bd31c8d73fe65ac36eaffb60
Charvis30 <97851133+Charvis30@users.noreply.github.com>
2023-05-01 17:19:45 +0000

    condensed code for rosters and teams

--- sportsApp/rosters/views.py
+++ sportsApp/rosters/views.py
@@ -49,19 +49,12 @@ def get_nba_schedule(team_abv):
 def nba_roster(request, team_abv):
     nba_roster = get_nba_roster(team_abv)
     nba_schedule = get_nba_schedule(team_abv)
-    game_date_str = "20230326"
-    game_date = datetime.strptime(game_date_str, "%Y%m%d")
+    
     context = {"nba_roster": nba_roster,
                "nba_schedule": nba_schedule,
-               "game_date": game_date}
+
+                }
     return render(request, f"nba-teams/{team_abv.lower()}.html", context)
-
-
-
-
-
-
-
 
 
 
@@ -82,7 +75,7 @@ def mlb_teams(request):
     return render(request, 'mlbteams.html', context)
 
 
-def get_team_roster(team_abv):
+def get_mlb_roster(team_abv):
     conn = http.client.HTTPSConnection("tank01-mlb-live-in-game-real-time-statistics.p.rapidapi.com")
     headers = {
         'X-RapidAPI-Key': "62179425a4mshc82870dfbd61b7cp115211jsne5fa10ef3b21",
@@ -93,155 +86,26 @@ def get_team_roster(team_abv):
     data = res.read()
     return json.loads(data.decode("utf-8"))
 
-def dbacks_roster(request):
-    team_roster = get_team_roster("ARI")
-    context = {"team_roster": team_roster}
-    return render(request, "mlb-teams/diamondbacks.html", context)
+def get_mlb_schedule(team_abv):
+    conn = http.client.HTTPSConnection("tank01-fantasy-stats.p.rapidapi.com")
+    headers = {
+        'X-RapidAPI-Key': "62179425a4mshc82870dfbd61b7cp115211jsne5fa10ef3b21",
+        'X-RapidAPI-Host': "tank01-fantasy-stats.p.rapidapi.com"
+    }
+    conn.request("GET", f"/getMLBTeamSchedule?teamAbv={team_abv}", headers=headers)
+    res = conn.getresponse()
+    data = res.read()
+    return json.loads(data.decode("utf-8"))
 
-def braves_roster(request):
-    team_roster = get_team_roster("ATL")
-    context = {"team_roster": team_roster}
-    return render(request, "mlb-teams/braves.html", context)
+def mlb_roster(request, team_abv):
+    mlb_roster = get_mlb_roster(team_abv)
+    mlb_schedule = get_mlb_schedule(team_abv)
+    context = {"mlb_roster": mlb_roster,
+               "nfl_schedule": mlb_schedule}
+    
+    return render(request, f"mlb-teams/{team_abv.lower()}MLB.html", context)
 
-def orioles_roster(request):
-    team_roster = get_team_roster("BAL")
-    context = {"team_roster": team_roster}
-    return render(request, "mlb-teams/orioles.html", context)
 
-def red_sox_roster(request):
-    team_roster = get_team_roster("BOS")
-    context = {"team_roster": team_roster}
-    return render(request, "mlb-teams/red_sox.html", context)
-
-def cubs_roster(request):
-    team_roster = get_team_roster("CHC")
-    context = {"team_roster": team_roster}
-    return render(request, "mlb-teams/cubs.html", context)
-
-def white_sox_roster(request):
-    team_roster = get_team_roster("CHW")
-    context = {"team_roster": team_roster}
-    return render(request, "mlb-teams/white_sox.html", context)
-
-def reds_roster(request):
-    team_roster = get_team_roster("CIN")
-    context = {"team_roster": team_roster}
-    return render(request, "mlb-teams/reds.html", context)
-
-def rockies_roster(request):
-    team_roster = get_team_roster("COL")
-    context = {"team_roster": team_roster}
-    return render(request, "mlb-teams/rockies.html", context)
-
-def tigers_roster(request):
-    team_roster = get_team_roster("DET")
-    context = {"team_roster": team_roster}
-    return render(request, "mlb-teams/tigers.html", context)
-
-def astros_roster(request):
-    team_roster = get_team_roster("HOU")
-    context = {"team_roster": team_roster}
-    return render(request, "mlb-teams/astros.html", context)
-
-def royals_roster(request):
-    team_roster = get_team_roster("KC")
-    context = {"team_roster": team_roster}
-    return render(request, "mlb-teams/royals.html", context)
-
-def angels_roster(request):
-    team_roster = get_team_roster("LAA")
-    context = {"team_roster": team_roster}
-    return render(request, "mlb-teams/angels.html", context)
-
-def dodgers_roster(request):
-    team_roster = get_team_roster("LAD")
-    context = {"team_roster": team_roster}
-    return render(request, "mlb-teams/dodgers.html", context)
-
-def marlins_roster(request):
-    team_roster = get_team_roster("MIA")
-    context = {"team_roster": team_roster}
-    return render(request, "mlb-teams/marlins.html", context)
-
-def brewers_roster(request):
-    team_roster = get_team_roster("MIL")
-    context = {"team_roster": team_roster}
-    return render(request, "mlb-teams/brewers.html", context)
-
-def twins_roster(request):
-    team_roster = get_team_roster("MIN")
-    context = {"team_roster": team_roster}
-    return render(request, "mlb-teams/twins.html", context)
-
-def mets_roster(request):
-    team_roster = get_team_roster("NYM")
-    context = {"team_roster": team_roster}
-    return render(request, "mlb-teams/mets.html", context)
-
-def yankees_roster(request):
-    team_roster = get_team_roster("NYY")
-    context = {"team_roster": team_roster}
-    return render(request, "mlb-teams/yankees.html", context)
-
-def athletics_roster(request):
-    team_roster = get_team_roster("OAK")
-    context = {"team_roster": team_roster}
-    return render(request, "mlb-teams/athletics.html", context)
-
-def phillies_roster(request):
-    team_roster = get_team_roster("PHI")
-    context = {"team_roster": team_roster}
-    return render(request, "mlb-teams/phillies.html", context)
-
-def pirates_roster(request):
-    team_roster = get_team_roster("PIT")
-    context = {"team_roster": team_roster}
-    return render(request, "mlb-teams/pirates.html", context)
-
-def padres_roster(request):
-    team_roster = get_team_roster("SD")
-    context = {"team_roster": team_roster}
-    return render(request, "mlb-teams/padres.html", context)
-
-def giants_roster(request):
-    team_roster = get_team_roster("SF")
-    context = {"team_roster": team_roster}
-    return render(request, "mlb-teams/giants.html", context)
-
-def mariners_roster(request):
-    team_roster = get_team_roster("SEA")
-    context = {"team_roster": team_roster}
-    return render(request, "mlb-teams/mariners.html", context)
-
-def cardinals_roster(request):
-    team_roster = get_team_roster("STL")
-    context = {"team_roster": team_roster}
-    return render(request, "mlb-teams/cardinals.html", context)
-
-def rays_roster(request):
-    team_roster = get_team_roster("TB")
-    context = {"team_roster": team_roster}
-    return render(request, "mlb-teams/rays.html", context)
-
-def blue_jays_roster(request):
-    team_roster = get_team_roster("TOR")
-    context = {"team_roster": team_roster}
-    return render(request, "mlb-teams/bluejays.html", context)
-
-def nationals_roster(request):
-    team_roster = get_team_roster("WAS")
-    context = {"team_roster": team_roster}
-    return render(request, "mlb-teams/nationals.html", context)
-
-def rangers_roster(request):
-    team_roster = get_team_roster("TEX")
-    context = {"team_roster": team_roster}
-    return render(request, "mlb-teams/rangers.html", context)
-
-def guardians_roster(request):
-    team_roster = get_team_roster("CLE")
-    context = {"team_roster": team_roster}
-    return render(request, "mlb-teams/guardians.html", context)
 
 
 ###########NFL################
@@ -271,10 +135,24 @@ def get_nfl_roster(team_abv):
     data = res.read()
     return json.loads(data.decode("utf-8"))
 
+def get_nfl_schedule(team_abv):
+    conn = http.client.HTTPSConnection("tank01-nfl-live-in-game-real-time-statistics-nfl.p.rapidapi.com")
+    headers = {
+        'X-RapidAPI-Key': "62179425a4mshc82870dfbd61b7cp115211jsne5fa10ef3b21",
+        'X-RapidAPI-Host': "tank01-nfl-live-in-game-real-time-statistics-nfl.p.rapidapi.com"
+    }
+    conn.request("GET", f"/getNFLTeamSchedule?teamAbv={team_abv}", headers=headers)
+    res = conn.getresponse()
+    data = res.read()
+    return json.loads(data.decode("utf-8"))
+
 
 def nfl_roster(request, team_abv):
     nfl_roster = get_nfl_roster(team_abv)
-    context = {"nfl_roster": nfl_roster}
+    nfl_schedule = get_nfl_schedule(team_abv)
+    context = {"nfl_roster": nfl_roster,
+               "nfl_schedule": nfl_schedule}
+    
     return render(request, f"nfl-teams/{team_abv.lower()}.html", context)
 
 
--- sportsApp/sportsApp/urls.py
+++ sportsApp/sportsApp/urls.py
@@ -39,24 +39,11 @@ from trending.views import(
 )
 
 from rosters.views import(
-    nba_teams, nba_roster,
+    nba_teams,
     mlb_teams,
     nfl_teams,
     nhl_teams,
 
-
-
-
-
-    orioles_roster, red_sox_roster, white_sox_roster, 
-    tigers_roster, astros_roster, royals_roster, angels_roster, twins_roster, 
-    yankees_roster, athletics_roster, mariners_roster, rays_roster, rangers_roster,
-    blue_jays_roster, dbacks_roster, braves_roster, cubs_roster, reds_roster, rockies_roster, 
-    dodgers_roster, marlins_roster, brewers_roster, mets_roster, phillies_roster, pirates_roster, padres_roster, 
-    giants_roster, cardinals_roster, nationals_roster, guardians_roster,
-
-    
-   
 )
 
 urlpatterns = [
@@ -78,53 +65,7 @@ urlpatterns = [
     path('news/', news, name="news"),
     path('trending/', trending, name='trending'),
 
-    #rosters (nba)
-    path('nbateams/', nba_teams, name='nbateams'),
-
-   
-
-    ##MLB##
-    path('mlbteams/', mlb_teams, name='mlbteams'),
-
-    path('yankees/', yankees_roster, name='Yankees'),
-    path('redsox/', red_sox_roster, name='Red Sox'),
-    path('orioles/', orioles_roster, name='Orioles'),
-    path('rays/', rays_roster, name='Rays'),
-    path('bluejays/', blue_jays_roster, name='Blue Jays'),
-    path('whitesox/', white_sox_roster, name='White Sox'),
-    path('tigers/', tigers_roster, name='Tigers'),
-    path('royals/', royals_roster, name='Royals'),
-    path('twins/', twins_roster, name='Twins'),
-    path('astros/', astros_roster, name='Astros'),
-    path('angels/', angels_roster, name='Angels'),
-    path('athletics/', athletics_roster, name='Athletics'),
-    path('mariners/', mariners_roster, name='Mariners'),
-    path('rangers/', rangers_roster, name='Rangers'),
-    path('braves/', braves_roster, name='Braves'),
-    path('marlins/', marlins_roster, name='Marlins'),
-    path('mets/', mets_roster, name='Mets'),
-    path('phillies/', phillies_roster, name='Phillies'),
-    path('nationals/', nationals_roster, name='Nationals'),
-    path('cubs/', cubs_roster, name='Cubs'),
-    path('reds/', reds_roster, name='Reds'),
-    path('brewers/', brewers_roster, name='Brewers'),
-    path('pirates/', pirates_roster, name='Pirates'),
-    path('cardinals/', cardinals_roster, name='Cardinals'),
-    path('diamondbacks/', dbacks_roster, name='Diamondbacks'),
-    path('rockies/', rockies_roster, name='Rockies'),
-    path('dodgers/', dodgers_roster, name='Dodgers'),
-    path('padres/', padres_roster, name='Padres'),
-    path('giants/', giants_roster, name='Giants'),
-    path('guardians/', guardians_roster, name='Guardians'),
-
-
-    ##NFL
-    path('nflteams/', nfl_teams, name='nflteams'),
-    
     path("rosters/", include("rosters.urls")),
-
-    ##NHL
-    path('nhlteams/', nhl_teams, name='nhlteams'),
 
 
     
